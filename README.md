1b35a5317c0191fb69ca3ca3175b0b45c4af63e3
zvictor <zvictor@users.noreply.github.com>
2021-11-01 20:09:35 +0100

    add the authorization header to the snapshot file

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 This lib let's you verify that all files in a given path have been uploaded to Google Photos.
 
 Step 0:
-Obtain an API key from https://developers.google.com/photos/library/reference/rest/v1/mediaItems/list
+* Obtain an API key from https://developers.google.com/photos/library/reference/rest/v1/mediaItems/list
+    1. Press `execute` on the right side frame ("Try this API").
+    1. Open the *Dev Console* and check for network activity.
+    1. The api key should be found as a query parameter in the url, such as `https://content-photoslibrary.googleapis.com/v1/mediaItems?key=xxxxxx`
+    1. The authorization key can be found as the `Authorization` header sent with the request call.
 
 Step 1:
-`deno run --allow-net --allow-write snapshot.ts YOUR_API_KEY`
+* Load and cache all media files in Google Photos:
+    * `deno run --allow-net --allow-write snapshot.ts YOUR_API_KEY AUTHORIZATION`
 
 Step 2:
-`deno run --allow-read index.ts FOLDER_WITH_MEDIA_FILES`
+* Check a local folder against the media data that has been fetched in step 1:
+    * `deno run --allow-read --unstable index.ts FOLDER_WITH_MEDIA_FILES`
--- snapshot.ts
+++ snapshot.ts
@@ -1,30 +1,41 @@
 import { sleep } from 'https://deno.land/x/sleep@v1.2.0/mod.ts'
 
-const load = async (key: string, nextPageToken?: string): Promise<unknown[]> => {
+const load = async (
+  key: string,
+  authorization?: string,
+  nextPageToken?: string
+): Promise<unknown[]> => {
   console.log(`Fetching page ${nextPageToken || 0}`)
+
+  const headers = {
+    accept: '*/*',
+    'accept-language': 'en,pt-BR;q=0.9,pt;q=0.8,en-US;q=0.7',
+    'cache-control': 'no-cache',
+    pragma: 'no-cache',
+    'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'x-goog-encode-response-if-executable': 'base64',
+    'x-javascript-user-agent': 'apix/3.0.0 google-api-javascript-client/1.1.0',
+    'x-origin': 'https://explorer.apis.google.com',
+    'x-referer': 'https://explorer.apis.google.com',
+    'x-requested-with': 'XMLHttpRequest',
+  }
+
+  if (authorization) {
+    // @ts-ignore
+    headers.authorization = authorization
+  }
 
   const response = await fetch(
     `https://content-photoslibrary.googleapis.com/v1/mediaItems?${
       nextPageToken ? `pageToken=${nextPageToken}&` : ''
     }pageSize=100&key=${key}`,
     {
-      headers: {
-        accept: '*/*',
-        'accept-language': 'en,pt-BR;q=0.9,pt;q=0.8,en-US;q=0.7',
-        'cache-control': 'no-cache',
-        pragma: 'no-cache',
-        'sec-ch-ua': '"Chromium";v="94", "Google Chrome";v="94", ";Not A Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'x-goog-encode-response-if-executable': 'base64',
-        'x-javascript-user-agent': 'apix/3.0.0 google-api-javascript-client/1.1.0',
-        'x-origin': 'https://explorer.apis.google.com',
-        'x-referer': 'https://explorer.apis.google.com',
-        'x-requested-with': 'XMLHttpRequest',
-      },
+      headers,
       referrerPolicy: 'strict-origin-when-cross-origin',
       body: null,
       method: 'GET',
@@ -34,19 +45,19 @@ const load = async (key: string, nextPageToken?: string): Promise<unknown[]> => 
   )
 
   const data = await response.json()
-  console.log(`${data.mediaItems.length} items loaded`)
+  console.log(`${data.mediaItems?.length} items loaded`)
 
   if (data.nextPageToken) {
     sleep(2)
-    return [...data.mediaItems, ...(await load(key, data.nextPageToken))]
+    return [...data.mediaItems, ...(await load(key, authorization, data.nextPageToken))]
   }
 
   return data.mediaItems
 }
 
 const main = async () => {
-  const [key] = Deno.args
-  const items = await load(key)
+  const [key, authorization] = Deno.args
+  const items = await load(key, authorization)
   Deno.writeTextFileSync('./data.json', JSON.stringify(items, null, 2))
 }
 
